51b52cef0dc45a7162179ed888e013045947e810
Igor Konovalov <ikonovi@gmail.com>
2022-07-16 12:21:48 +0300

    Account model can be compared on equals

--- src/main/java/com/vrp/task_sf_simple/models/Account.java
+++ src/main/java/com/vrp/task_sf_simple/models/Account.java
@@ -1,5 +1,7 @@
 package com.vrp.task_sf_simple.models;
 
+import java.util.Objects;
+
 public class Account {
     private String name;
     private String phone;
@@ -17,9 +19,34 @@ public class Account {
     }
 
     public class Builder {
-        private Builder() {}
-        public Builder name(String name) { Account.this.name = name; return this; }
-        public Builder phone(String phone) { Account.this.phone = phone; return this; }
-        public Account build() {return Account.this; }
+        private Builder() {
+        }
+
+        public Builder name(String name) {
+            Account.this.name = name;
+            return this;
+        }
+
+        public Builder phone(String phone) {
+            Account.this.phone = phone;
+            return this;
+        }
+
+        public Account build() {
+            return Account.this;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(name, account.name) && Objects.equals(phone, account.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, phone);
     }
 }
