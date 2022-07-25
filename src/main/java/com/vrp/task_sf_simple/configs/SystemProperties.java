8a31beb7af2baa53781ca58fad72c0a7ad1ec6c4
Igor Konovalov <ikonovi@gmail.com>
2022-07-25 11:42:55 +0300

    handled NPE when Sys Prop is not defined

--- src/main/java/com/vrp/task_sf_simple/configs/SystemProperties.java
+++ src/main/java/com/vrp/task_sf_simple/configs/SystemProperties.java
@@ -1,9 +1,10 @@
 package com.vrp.task_sf_simple.configs;
 
 public final class SystemProperties {
-    public static final String BASE_URL = System.getProperty("HOST");
-    public static final String USER_NAME = System.getProperty("USER");
-    public static final String PASSWORD = System.getProperty("PWD");
+    private static final String NOT_DEFINED = "NotDefined";
+    public static final String BASE_URL = System.getProperty("HOST", NOT_DEFINED);
+    public static final String USER_NAME = System.getProperty("USER", NOT_DEFINED);
+    public static final String PASSWORD = System.getProperty("PWD", NOT_DEFINED);
 
     private SystemProperties() {
     }
